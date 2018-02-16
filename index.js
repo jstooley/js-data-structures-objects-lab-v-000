9600a34dc77b13aa6546c63770b04aa9a6a62ef1
Justin Tooley <jstooley823@gmail.com>
2018-02-16 03:32:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const driver = {};
 
-function updateDriverWithKetAndValie(driver,key,value){
+function updateDriverWithKeyAndValue(driver, key, value){
       return Object.assign({},driver, {[key]: value});
 
 }
